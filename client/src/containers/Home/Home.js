538f1d8d1411828e2f012104dba7896507592ea2
Erik Schake <s4ekscha@uni-trier.de>
2020-03-21 16:13:00 +0100

    Client: Added UI Elements

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,10 +1,13 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class Home extends Component {
 
     render() {
         return (
-            <div>Home</div>
+            <Fragment>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate </p>
+                
+            </Fragment>
         );
     }
 
