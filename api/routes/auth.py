f6b09dbd9b2bdb504d0d075fe6f8b2289819a5d6
Erik Schake <erik.schake@gmail.com>
2020-04-26 15:33:34 +0200

    Added /signin unit tests

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -17,49 +17,7 @@ def signup(
     usage_purpose: str = Body(...)
 ):
     try:
-        user_exists, _ = UsersDb.username_exists(username)
-        if user_exists:
-            raise UserAlreadyExistsException(f'Bad request: {username} already exists.')
-
-        new_user: UserStored = UserStored(
-            username = username,
-            password = auth.hash_pw(password),
-            time_created = auth.get_timestamp(),
-            usage_purpose=usage_purpose
-        )
-        user_id = auth.generate_uuid()
-
-        access_token: str = auth.generate_access_token(user_id, new_user)
-        header, payload, signature = access_token.decode().split('.')
-
-        response.set_cookie(
-            key=auth.AUTH_CONFIG['access_token']['body_cookie_key'],
-            value='.'.join((header, payload)),
-            secure=True,
-            # samesite='strict' !!! not supported by fastapi at the moment. Dependent on lower starlette version
-        )
-        response.set_cookie(
-            key=auth.AUTH_CONFIG['access_token']['signature_cookie_key'],
-            value=signature,
-            secure=True,
-            httponly=True,
-            # samesite='strict'
-        )
-
-        #set samesite flag manually
-        for idx, header in enumerate(response.raw_headers):
-            if header[0].decode('utf-8') == 'set-cookie':
-                cookie: str = header[1].decode('utf-8')
-                if cookie.startswith(auth.AUTH_CONFIG['access_token']['body_cookie_key']) or cookie.startswith(auth.AUTH_CONFIG['access_token']['signature_cookie_key']):
-                    cookie = cookie + '; SameSite=Strict'
-                    response.raw_headers[idx] = (header[0], cookie.encode())
-
-        UsersDb.save_new_user(user_id, new_user)
-        
-        return UserLoginBody(
-            username=username,
-            expires_in=auth.AUTH_CONFIG['access_token']['lifetime']
-        )
+        return do_signup_logic(response, username, password, usage_purpose)
 
     except UserAlreadyExistsException as err:
         print(err)
@@ -81,3 +39,67 @@ def signup(
                 message='Something went wrong'
             )
         )
+
+@router.post('/signin', response_model=UserLoginBody)
+def signin(
+    response: Response,
+    username: str = Body(...),
+    password: str = Body(...)
+):
+    return JSONResponse(content={'test': 'test'})
+    
+def do_signup_logic(
+    response: Response,
+    username: str,
+    password: str,
+    usage_purpose: str
+) -> UserLoginBody:
+
+    # check if user already exists
+    user_exists, _ = UsersDb.username_exists(username)
+    if user_exists:
+        raise UserAlreadyExistsException(f'Bad request: {username} already exists.')
+
+    # create new user
+    new_user: UserStored = UserStored(
+        username = username,
+        password = auth.hash_pw(password),
+        time_created = auth.get_timestamp(),
+        usage_purpose=usage_purpose
+    )
+    user_id = auth.generate_uuid()
+
+    # create access token
+    access_token: str = auth.generate_access_token(user_id, new_user)
+    header, payload, signature = access_token.decode().split('.')
+
+    # set token in cookies
+    response.set_cookie(
+        key=auth.AUTH_CONFIG['access_token']['body_cookie_key'],
+        value='.'.join((header, payload)),
+        secure=True,
+        # samesite='strict' !!! not supported by fastapi at the moment. Dependent on lower starlette version
+    )
+    response.set_cookie(
+        key=auth.AUTH_CONFIG['access_token']['signature_cookie_key'],
+        value=signature,
+        secure=True,
+        httponly=True,
+        # samesite='strict'
+    )
+
+    #set samesite flag manually
+    for idx, header in enumerate(response.raw_headers):
+        if header[0].decode('utf-8') == 'set-cookie':
+            cookie: str = header[1].decode('utf-8')
+            if cookie.startswith(auth.AUTH_CONFIG['access_token']['body_cookie_key']) or cookie.startswith(auth.AUTH_CONFIG['access_token']['signature_cookie_key']):
+                cookie = cookie + '; SameSite=Strict'
+                response.raw_headers[idx] = (header[0], cookie.encode())
+
+    # save newly created user only if all steps up until now were successful
+    UsersDb.save_new_user(user_id, new_user)
+    
+    return UserLoginBody(
+        username=username,
+        expires_in=auth.AUTH_CONFIG['access_token']['lifetime']
+    )
--- api/test_auth.py
+++ api/test_auth.py
@@ -2,14 +2,17 @@ import os
 import unittest
 from typing import List
 import jwt
+import random
+import string
 
 # For local testing, the Firebase credentials are provided from a local file,
 # see WHREG-8. We need to set the environment variable BEFORE we
 # import our app. Otherwise we receive a "missing credentials" exception.
 from starlette.testclient import TestClient
+from starlette.responses import Response
 
 from models import User, Anamnesis, UserStored
-from auth.functions import AUTH_CONFIG, hash_pw, generate_uuid
+from auth.functions import AUTH_CONFIG, hash_pw, generate_uuid, get_timestamp
 
 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'firebase_key.json'
 # We need to suppress the PEP8 error 'import not at top'
@@ -23,7 +26,7 @@ PURPOSE_PRIVATE_ID = u'NnqnZ38FMOUFskHFEjUX'
 os.environ['PORT'] = '8080'
 
 
-class TestApi(unittest.TestCase):
+class TestAuth(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         self._bin = []
@@ -34,30 +37,136 @@ class TestApi(unittest.TestCase):
         from api.app import app
         self.client = TestClient(app)
         # Create a document in the database for the test user.
-        user = User(username=TEST_USER_NAME)
-        UsersDb.set_user(user,
-                         anamnesis=Anamnesis(
-                                 user=user,
-                                 gender='male',
-                                 residence=22765,
-                                 birthyear=1951
-                                 ))
+        UsersDb.save_new_user(
+            user_id=generate_uuid(),
+            user=UserStored(
+                username=TEST_USER_NAME,
+                password=hash_pw(TEST_USER_PW),
+                time_created=get_timestamp(),
+                usage_purpose=PURPOSE_PRIVATE_ID
+            )
+        )
 
     def tearDown(self):
         """Test teardown; is called after every unit test"""
         UsersDb.remove_user_by_username(TEST_USER_NAME)
+        UsersDb.remove_user_by_username(TEST_USER_NAME+'NEW')
 
     def test_sign_up_new_user_successful(self):
         post_body = {
-            'username': TEST_USER_NAME,
+            'username': TEST_USER_NAME+'NEW',
             'password': TEST_USER_PW,
             'usage_purpose': PURPOSE_PRIVATE_ID
         }
         res = self.client.post('/auth/signup', json=post_body)
         
         assert res.status_code == 200
+        TestAuth.check_login_response(res, user_name_appendix='NEW')
+
+    def test_sign_up_user_exists(self):
+        post_body = {
+            'username': TEST_USER_NAME,
+            'password': TEST_USER_PW,
+            'usage_purpose': PURPOSE_PRIVATE_ID
+        }
+        res = self.client.post('/auth/signup', json=post_body)
+        assert res.status_code == 400
+        TestAuth.check_error_response(400, res.json(), 'USER_EXISTS')
+
+    def test_signup_missing_field(self):
+        post_body_1 = {
+            'password': TEST_USER_PW,
+            'usage_purpose': PURPOSE_PRIVATE_ID
+        }
+        post_body_2 = {
+            'usage_purpose': PURPOSE_PRIVATE_ID
+        }
+        post_body_3 = {
+        }
+        res = self.client.post('/auth/signup', json=post_body_1)
+        assert res.status_code == 422
+        TestAuth.check_err_body_param(res, 'username', 'value_error.missing')
+        res = self.client.post('/auth/signup', json=post_body_2)
+        assert res.status_code == 422
+        TestAuth.check_err_body_param(res, 'username', 'value_error.missing')
+        TestAuth.check_err_body_param(res, 'password', 'value_error.missing')
+        res = self.client.post('/auth/signup', json=post_body_3)
+        assert res.status_code == 422
+        TestAuth.check_err_body_param(res, 'username', 'value_error.missing')
+        TestAuth.check_err_body_param(res, 'password', 'value_error.missing')
+        TestAuth.check_err_body_param(res, 'usage_purpose', 'value_error.missing')
+
+    def test_signup_pw_too_short(self):
+        post_body = {
+            'username': TEST_USER_NAME,
+            'password': '123',
+            'usage_purpose': PURPOSE_PRIVATE_ID
+        }
+        res = self.client.post('/auth/signup', json=post_body)
+        assert res.status_code == 422
+        TestAuth.check_err_body_param(res, 'password', 'value_error.any_str.min_length')
+
+    def test_sign_in_successful(self):
+        post_body = {
+            'username': TEST_USER_NAME,
+            'password': TEST_USER_PW
+        }
+        res = self.client.post('/auth/signin', post_body)
+        assert res.status_code == 200
+        TestAuth.check_login_response(res)
+
+    def test_signin_wrong_password(self):
+        post_body = {
+            'username': TEST_USER_NAME,
+            'password': TEST_USER_PW+'WRONG'
+        }
+        res = self.client.post('/auth/signin', post_body)
+        assert res.status_code == 400
+        TestAuth.check_error_response(400, res.json(), 'WRONG_PASSWORD')
+
+    def test_signin_user_not_exists(self):
+        post_body = {
+            'username': TEST_USER_NAME + ''.join((random.choice(string.ascii_lowercase) for _ in range(10))),
+            'password': TEST_USER_PW
+        }
+        res = self.client.post('/auth/signin', post_body)
+        assert res.status_code == 400
+        TestAuth.check_error_response(400, res.json(), 'USER_NOT_FOUND')
+
+    def test_signin_missing_field(self):
+        post_body_1 = {
+            'password': TEST_USER_PW
+        }
+        post_body_3 = {
+        }
+        res = self.client.post('/auth/signin', json=post_body_1)
+        assert res.status_code == 422
+        TestAuth.check_err_body_param(res, 'username', 'value_error.missing')
+        res = self.client.post('/auth/signin', json=post_body_3)
+        assert res.status_code == 422
+        TestAuth.check_err_body_param(res, 'username', 'value_error.missing')
+        TestAuth.check_err_body_param(res, 'password', 'value_error.missing')
+
+    @staticmethod
+    def check_error_response(status: int, res_body: dict, key: str) -> None:
+        assert 'error' in res_body
+        assert res_body['error']['code'] == status
+        assert res_body['error']['key'] == key
+
+    @staticmethod
+    def check_err_body_param(res: Response, field: str, type: str) -> None:
         res_body = res.json()
-        assert res_body['username'] == TEST_USER_NAME
+        assert 'detail' in res_body
+        contained = False
+        for d in res_body['detail']:
+            if 'body' in d['loc'] and field in d['loc'] and d['type'] == type:
+                contained = True
+        assert contained == True
+
+    @staticmethod
+    def check_login_response(res: Response, user_name_appendix: str = '') -> None:
+        res_body = res.json();
+        assert res_body['username'] == TEST_USER_NAME+'NEW'
         assert res_body['expires_in'] == AUTH_CONFIG['access_token']['lifetime']
         # assert cookies
         cookies = res.headers['set-cookie'].split(',')
@@ -72,26 +181,4 @@ class TestApi(unittest.TestCase):
                 cnt += 1
         assert cnt == 2, 'Not both cookies could be found'
         dec = jwt.decode(token, AUTH_CONFIG['access_token']['secret'], algorithms=AUTH_CONFIG['access_token']['sign_alg'])
-        assert dec['username'] == TEST_USER_NAME
-
-    def test_sign_up_user_exists(self):
-        new_user = UserStored(
-            username=TEST_USER_NAME,
-            password=hash_pw(TEST_USER_PW),
-            time_created=10000,
-            usage_purpose=PURPOSE_PRIVATE_ID
-        )
-        user_id = generate_uuid()
-        UsersDb.save_new_user(user_id, new_user)
-
-        post_body = {
-            'username': TEST_USER_NAME,
-            'password': TEST_USER_PW,
-            'usage_purpose': PURPOSE_PRIVATE_ID
-        }
-        res = self.client.post('/auth/signup', json=post_body)
-        assert res.status_code == 400
-        res_body = res.json()
-        assert 'error' in res_body
-        assert res_body['error']['code'] == 400
-        assert res_body['error']['key'] == 'USER_EXISTS'
+        assert dec['username'] == TEST_USER_NAME+user_name_appendix
